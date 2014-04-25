ce76af13df8310ff93be333cdffcfb0df1335885
James Nugent <james.nugent@bathasu.com>
2014-04-25 21:15:27 +0100

    ConEmu settings

--- scripts/conemu.ps1
+++ scripts/conemu.ps1
@@ -4,6 +4,11 @@ $conemuArgs = '/p:x64 /quiet ADDLOCAL="FConEmuGui,FConEmuGui64,ProductFeature,FC
 Write-Host "Installing ConEmu"
 Start-Process -FilePath $conemuSetup -ArgumentList $conemuArgs -NoNewWindow -Wait
 
+Write-Host "Installing Sane Defaults for ConEmu"
+$defaultsPath = "C:\Users\vagrant\ConEmu.reg"
+Start-Process -FilePath "reg" -ArgumentList "import $defaultsPath" -NoNewWindow -Wait
+Remove-Item -Force -Path $defaultsPath
+
 Write-Host "Pinning ComEmu to the TaskBar"
 $shell = new-object -com "Shell.Application"
 $dir = $shell.Namespace("C:\Program Files\ConEmu")
