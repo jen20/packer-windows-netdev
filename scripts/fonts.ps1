d504f4752e9e5f11ac7b70b6d6bf2e71a16525d9
James Nugent <james.nugent@bathasu.com>
2014-04-25 20:09:27 +0100

    Add font installation script

---
+++ scripts/fonts.ps1
@@ -0,0 +1,13 @@
+$shell = New-Object -com "Shell.Application"
+$zip = $shell.NameSpace("C:\Users\vagrant\SourceCodePro_FontsOnly-1.017.zip")
+$destination = "C:\Users\vagrant\"
+foreach($item in $zip.items())
+{
+    $shell.Namespace($destination).CopyHere($item)
+}
+
+$fontPath = Join-Path (Join-Path $destination "SourceCodePro_FontsOnly-1.017") "TTF"
+$dir = $shell.NameSpace($fontPath)
+Get-ChildItem -Path $fontPath -filter "*.ttf" | ForEach {
+    $dir.ParseName($_.Name).InvokeVerb("Install")
+}
