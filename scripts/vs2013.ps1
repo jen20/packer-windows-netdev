ecebde0840c76fd33931f09d5117f1109a5f7778
James Nugent <james.nugent@bathasu.com>
2014-04-25 16:38:52 +0100

    Correct voice of the log messages

--- scripts/vs2013.ps1
+++ scripts/vs2013.ps1
@@ -2,7 +2,7 @@ $isoPath = "C:\users\vagrant\en_visual_studio_premium_2013_x86_dvd_3175275.iso"
 $rc = Mount-DiskImage -PassThru -ImagePath $isoPath
 $driveLetter = ($rc | Get-Volume).DriveLetter
 
-Write-Host "Install Visual Studio"
+Write-Host "Installing Visual Studio"
 $installPath = Join-Path "${driveLetter}:" "vs_premium.exe"
 Start-Process -FilePath $installPath -ArgumentList "/adminfile A:\AdminDeployment.xml /quiet /norestart" -NoNewWindow -Wait
 
@@ -10,20 +10,20 @@ Dismount-DiskImage -ImagePath $isoPath
 Remove-Item -Force -Path $isoPath 
 
 
-Write-Host "Install Resharper"
+Write-Host "Installing Resharper"
 $resharperInstallerPath = "C:\Users\vagrant\ReSharperSetup.8.2.0.2160.msi"
 Start-Process -FilePath $resharperInstallerPath -ArgumentList "/qn" -Wait
 Remove-Item -Force -Path $resharperInstallerPath
 
 
-Write-Host "Install the Hide Main Menu VSIX"
+Write-Host "Installing the Hide Main Menu VSIX"
 $vsixInstallerPath = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\VSIXInstaller.exe"
 $extensionPath = "c:\users\vagrant\HideMenu.vsix"
 Start-Process -FilePath $vsixInstallerPath -ArgumentList "/q $extensionPath" -NoNewWindow -Wait
 Remove-Item -Force -Path $extensionPath
 
 
-Write-Host "Import some sensible defaults to Visual Studio and kill the first run wizard"
+Write-Host "Importing some sensible defaults to Visual Studio and killing the first run wizard"
 $settingsPath = "C:\Users\vagrant\Sane.vssettings"
 $devenvPath = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
 $process = Start-Process -FilePath $devenvPath -ArgumentList "/ResetSettings $settingsPath /Command File.Exit" -NoNewWindow -PassThru
@@ -34,11 +34,11 @@ if ( ! $process.WaitForExit(60000) ) {
 Remove-Item -Force -Path $settingsPath
 
 
-Write-Host "FIX THE ALL CAPS MENU IN VISUAL STUDIO"
+Write-Host "FIXING THE ALL CAPS MENU IN VISUAL STUDIO"
 Set-ItemProperty -Path HKCU:\Software\Microsoft\VisualStudio\12.0\General -Name SuppressUppercaseConversion -Type DWord -Value 1
 
 
-Write-Host "Configure Resharper to use the IntelliJ Keyboard Scheme"
+Write-Host "Configuring Resharper to use the IntelliJ Keyboard Scheme"
 $dotSettingsSource = "C:\Users\vagrant\vsActionManager.DotSettings"
 $dotSettingsDestination = "C:\Users\vagrant\AppData\Local\JetBrains\ReSharper\vAny\vs12.0"
 New-Item $dotSettingsDestination -Type directory
