2e2244ace7ab80d4c8e36b0d8530b82ec5528c77
James Nugent <james.nugent@bathasu.com>
2014-04-26 16:24:58 +0100

    Do better on DPI, disable edge help

---
+++ scripts/NoStupidHelp.bat
@@ -0,0 +1,2 @@
+reg.exe ADD "HKCU\Software\Policies\Microsoft\Windows\EdgeUI" /v DisableHelpSticker /t REG_DWORD /d 1 /f
+reg.exe ADD "HKLM\Software\Policies\Microsoft\Windows\EdgeUI" /v DisableHelpSticker /t REG_DWORD /d 1 /f
--- scripts/dpi.bat
+++ scripts/dpi.bat
@@ -1,1 +1,3 @@
-reg.exe ADD HKCC\Software\Fonts\ /v LogPixels /t REG_DWORD /d 144 /f
+reg.exe ADD "HKCU\Control Panel\Desktop" /v Win8DpiScaling /t REG_DWORD /d 1 /f
+reg.exe ADD "HKCU\Control Panel\Desktop" /v LogPixels /t REG_DWORD /d 144 /f
+reg.exe ADD "HKCU\Control Panel\Desktop" /v LogicalDPIOverride /t REG_DWORD /d 1 /f
